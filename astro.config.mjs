67e8733b3077debceab915e29812db1208ddd5f3
Chris Swithinbank <swithinbank@gmail.com>
2024-07-07 02:41:10 +0200

    Configure `site`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+/* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
+const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
+
 // https://astro.build/config
 export default defineConfig({
+	site: NETLIFY_PREVIEW_SITE || 'https://starlight-examples.netlify.app/',
 	integrations: [
 		starlight({
 			title: 'Starlight Examples',
