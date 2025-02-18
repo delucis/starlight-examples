53b9e6a744e7c56a528b801cb9d724cf7ab4af1d
Chris Swithinbank <swithinbank@gmail.com>
2025-02-18 14:51:30 +0100

    Add first two examples locally for testing

---
+++ examples/css-icons/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      customCss: ['./src/styles.css'],
+    }),
+  ],
+});
