226aebfc108f8c660579ac6d0e0a66d96f275858
Chris Swithinbank <swithinbank@gmail.com>
2025-02-19 23:41:23 +0100

    Remove unused variable in multi-sidebar middleware

--- examples/multi-sidebar/src/routeMiddleware.ts
+++ examples/multi-sidebar/src/routeMiddleware.ts
@@ -5,7 +5,7 @@ export const onRequest = defineRouteMiddleware((context) => {
 	// e.g. `/product-2/some-page/` returns `/product-2/`
 	const currentBase = context.url.pathname.split('/').slice(0, 2).join('/') + '/';
 
-	const { pagination, entry } = context.locals.starlightRoute;
+	const { pagination } = context.locals.starlightRoute;
 
 	// Filter our sidebar groups that do not include links to the current product.
 	context.locals.starlightRoute.sidebar = context.locals.starlightRoute.sidebar.filter(
