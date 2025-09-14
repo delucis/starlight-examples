82a11362b7042f0afa1c5af333ae064a2898d6f7
Chris Swithinbank <swithinbank@gmail.com>
2025-09-14 10:52:52 +0200

    Add code snippet example

---
+++ examples/transclusion-code/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: '<Snippet>',
+    }),
+  ],
+});
